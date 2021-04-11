28263e70710e142b403bf9c4fefae58f7f3e8334
Alaa Elnagar <35043356+AlaaElnagar@users.noreply.github.com>
2021-04-11 18:38:12 +0200

    Project4

---
+++ Project4/catkin_ws (1)/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,22 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src
+# Build directory: /home/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(slam_gmapping/slam_gmapping)
+subdirs(teleop_twist_keyboard)
+subdirs(turtlebot/turtlebot)
+subdirs(turtlebot/turtlebot_bringup)
+subdirs(turtlebot/turtlebot_capabilities)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
+subdirs(ball_chaser)
+subdirs(pgm_map_creator)
+subdirs(slam_gmapping/gmapping)
+subdirs(turtlebot/turtlebot_teleop)
+subdirs(turtlebot/turtlebot_description)
